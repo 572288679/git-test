2baf62e4efb17cfd52059cb061d3de53524746df
Ailixiya <572288679@qq.com>
2022-10-14 21:32:30 +0800

    second commit

--- src/main/java/com/git/GItTest.java
+++ src/main/java/com/git/GItTest.java
@@ -6,5 +6,7 @@ package com.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+
     }
 }
